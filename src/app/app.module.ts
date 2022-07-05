161d988f77a30b4fe15182a14279ec7c7abf53d2
Rhys Woolcott <rhyswoolcott@gmail.com>
2022-07-06 00:13:38 +0100

    angular 13 update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, FontAwesomeModule],
+  imports: [BrowserModule, FontAwesomeModule],
   exports: [],
   providers: [Title],
   bootstrap: [AppComponent],
